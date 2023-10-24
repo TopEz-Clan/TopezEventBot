ac45212e5c53297bd98abe7a90ce556b1798739d
Sebastian Schubert <sschubert932@gmail.com>
2023-10-25 00:14:47 +0200

    Add notificationsent flag to SchedulableEvent.cs

---
+++ TopezEventBot.Data/Migrations/20231024221430_SchedulableEventAddNotificationSentFlag.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TopezEventBot.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SchedulableEventAddNotificationSentFlag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "NotificationSent",
+                table: "SchedulableEvents",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NotificationSent",
+                table: "SchedulableEvents");
+        }
+    }
+}
